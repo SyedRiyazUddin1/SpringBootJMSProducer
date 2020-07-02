1d0ee9c9292b8542d6eaa8284320f8317e24a97d
SyedRiyazUddin1 <59564957+SyedRiyazUddin1@users.noreply.github.com>
2020-07-02 20:19:51 +0530

    Add files via upload

---
+++ SpringBJMSConsumer-productobject/src/main/java/com/myspringboot/SpringBjmsConsumerProductobjectApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBjmsConsumerProductobjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBjmsConsumerProductobjectApplication.class, args);
+	}
+
+}
